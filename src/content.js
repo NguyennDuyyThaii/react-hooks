432fd74f0047525569ded23b41d5587b6537a77e
thai.nguyenduy@powergatesoftware.com <thai.nguyenduy@powergatesoftware.com>
2023-02-13 16:41:54 +0700

    react useEffect

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-// 1. Update DOM 
+// 1. Update DOM
 // 2. Call API
 // 3. Listen DOM events
 //  - scroll
@@ -9,37 +9,72 @@ import { useEffect, useState } from "react"
 //  - Remove listener / unsubcribe
 //  - Clear timer
 
-    // useEffect(callback)
-    // - Goi callback moi khi component re-render
-    // - goi callback sau kho component them element vao dom
-    // useEffect(callback, [])
-    // - Chi goi callback 1 lan sau khi component mount
-    // useEffect(callback, [deps])
+// useEffect(callback)
+// - Goi callback moi khi component re-render
+// - goi callback sau kho component them element vao dom
+// useEffect(callback, [])
+// - Chi goi callback 1 lan sau khi component mount
+// useEffect(callback, [deps])
+// - callback se duoc goi lai moi khi deps thay doi
 
-    // 1. Callback luon duoc goi lai sau khi component mounted
-const tabs = ['posts', 'comments', 'albums']
-function Content(){
-    const [title, setTitle] = useState('')
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(posts => {
-            setPosts(posts)
-        })
-    },[])
+// 1. Callback luon duoc goi lai sau khi component mounted
+// 2. Clearnup function luon duoc goi truoc khi component unmounted
+const tabs = ["posts", "comments", "albums"];
+function Content() {
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [type, setType] = useState('posts');
+  const [showGoToTop, setShowGoToTop] = useState(false)
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((res) => res.json())
+      .then((posts) => {
+        setPosts(posts);
+      });
+  }, [type]);
 
-    return (
-        // <h1>Mount and UnMount</h1>
-        <div>
-            <input value={title} onChange={e => setTitle(e.target.value)} />
-        <ul>
-            {posts.map(item => (
-                <li key={item.id}>{item.title}</li>
-            ))}
-        </ul>
-        </div>
-    )
+  useEffect(() => {
+    const handleScroll = () => {
+        setShowGoToTop(window.scrollY >=200)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    // clearnup function
+
+    return () => [
+        window.removeEventListener('scroll', handleScroll)
+    ]
+  }, [])
+
+  return (
+    // <h1>Mount and UnMount</h1>
+    <div>
+        {
+            tabs.map(tab => (
+                <button 
+                onClick={() => setType(tab)}
+                key={tab}
+                style={type === tab ? {
+                    color: '#fff',
+                    backgroundColor: '#333'
+                } : {}}
+                >{tab}</button>
+            ))
+        }
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+
+        {showGoToTop && (
+            <button style={{position: 'fixed', right: 20, bottom:20}}>
+Go to the top
+            </button>
+  )}
+      </ul>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
