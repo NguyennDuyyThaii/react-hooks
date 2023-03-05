809493d33c0c455a2253d179acaf2c107550d61f
Benjamin Smith <benjamin_smith@Benjamins-MacBook-Pro.local>
2023-03-05 23:33:09 +0700

    useEffect vn

--- src/content.js
+++ src/content.js
@@ -19,37 +19,102 @@ import { useEffect, useState } from "react";
 
 // 1. Callback luon duoc goi lai sau khi component mounted
 // 2. Clearnup function luon duoc goi truoc khi component unmounted
+// 3. Clearnup function luon duoc goi truoc khi callback duoc goi(tru lan mounted)
 const tabs = ["posts", "comments", "albums"];
+const lessons = [
+  {
+    id: 1,
+    name: 'React la gi'
+  },
+
+  {
+    id: 2,
+    name: 'Node la gi'
+  },
+
+  {
+    id: 3,
+    name: 'PHP la gi'
+  }
+]
 function Content() {
-  const [title, setTitle] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState('posts');
-  const [showGoToTop, setShowGoToTop] = useState(false)
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
-      });
-  }, [type]);
+//   const [title, setTitle] = useState("");
+//   const [posts, setPosts] = useState([]);
+//   const [type, setType] = useState('posts');
+//   const [showGoToTop, setShowGoToTop] = useState(false)
+//   useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/${type}`)
+//       .then((res) => res.json())
+//       .then((posts) => {
+//         setPosts(posts);
+//       });
+//   }, [type]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-        setShowGoToTop(window.scrollY >=200)
-    }
+//   useEffect(() => {
+//     const handleScroll = () => {
+//         setShowGoToTop(window.scrollY >=200)
+//     }
 
-    window.addEventListener('scroll', handleScroll)
-    // clearnup function
+//     window.addEventListener('scroll', handleScroll)
+//     // clearnup function
 
-    return () => [
-        window.removeEventListener('scroll', handleScroll)
-    ]
-  }, [])
+//     return () => [
+//         window.removeEventListener('scroll', handleScroll)
+//     ]
+//   }, [])
 
+    // useEffect with timmer
+
+    // const [countdown, setCountDown]  = useState(180)
+
+    // useEffect(() => {
+    //     const timeId = setTimeout(() => {
+    //         setCountDown(countdown - 1)
+    //     }, 1000)
+
+    //     return () => clearInterval(timeId)
+    // }, [countdown])
+
+    // useEffect with preview avatar
+
+    // const [avatar, setAvatar] = useState()
+    // useEffect(() => {
+    //   // ussing cleanup 
+
+    //   return () => {
+    //     avatar && URL.revokeObjectURL(avatar.preview)
+    //   }
+    // }, [avatar])
+    // const handlePreviewAvatar = (e) => {
+
+    //   const file = e.target.files[0]
+
+    //   file.preview = URL.createObjectURL(file)
+    //   setAvatar(file)
+
+    //   // e.target.value = null
+
+
+    // useEffect with fake chat app
+
+    const [lessonId, setLessonId] = useState(1)
+
+    useEffect(() => {
+      const handleComment = ({detail}) => {
+        console.log(detail)
+      }
+      window.addEventListener(`lession-${lessonId}`, handleComment)
+
+      return () => {
+        window.removeEventListener(`lession-${lessonId}`, handleComment)
+      }
+
+    }, [lessonId])
+    } 
   return (
     // <h1>Mount and UnMount</h1>
     <div>
-        {
+        {/* {
             tabs.map(tab => (
                 <button 
                 onClick={() => setType(tab)}
@@ -72,9 +137,33 @@ function Content() {
 Go to the top
             </button>
   )}
+      </ul> */}
+
+      {/* useEffect with timmer */}
+      {/* <h1>{countdown}</h1> */}
+
+      {/* useEffect with preview avatar */}
+{/* 
+      <input type="file" onChange={handlePreviewAvatar} />
+      {avatar && (
+        <img src={avatar.preview} alt = "" width="80%" />
+      )} */}
+
+      {/* useEffect with fake chat App */}
+
+      <ul>
+        {lessons.map(lesson => (
+          <li
+          key = {lesson.id}
+          style = {{color: lessonId === lesson.id ? 'red' : '#333'}} 
+          onClick = {() => setLessonId(lesson.id)}
+          >
+            {lesson.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
-}
+
 
 export default Content;
